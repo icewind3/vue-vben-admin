56b1e1804dd496fcb133635e04cf0ded5f84310f
yejianyu <ye_jiany@163.com>
2025-11-26 17:15:31 +0800

    refactor: 删除无用模块

--- apps/web-antd/src/api/core/auth.ts
+++ apps/web-antd/src/api/core/auth.ts
@@ -1,4 +1,18 @@
+import qs from 'qs';
+
 import { baseRequestClient, requestClient } from '#/api/request';
+import { APP } from '#/preferences';
+
+export const ContentTypeEnum = {
+  json: 'application/json;charset=UTF-8',
+  formData: 'multipart/form-data;charset=UTF-8',
+  formURLEncoded: 'application/x-www-form-urlencoded;charset=UTF-8',
+};
+export enum GrantTypeEnum {
+  DEPT = 'dept',
+  PASSWORD = 'password',
+  REFRESH_TOKEN = 'refresh_token',
+}
 
 export namespace AuthApi {
   /** 登录接口参数 */
@@ -18,11 +32,29 @@ export namespace AuthApi {
   }
 }
 
+const createAuthHeader = () => {
+  const { client_id, client_secret } = APP.OAUTH;
+  return {
+    'content-type': ContentTypeEnum.formURLEncoded,
+
+    Authorization: `basic ${window.btoa(`${client_id}:${client_secret}`)}`,
+  };
+};
+
 /**
  * 登录
  */
 export async function loginApi(data: AuthApi.LoginParams) {
-  return requestClient.post<AuthApi.LoginResult>('/auth/login', data);
+  return baseRequestClient.post<AuthApi.LoginResult>(
+    '/oauth2/token',
+    qs.stringify(
+      { ...data, grant_type: GrantTypeEnum.PASSWORD },
+      { arrayFormat: 'brackets' },
+    ),
+    {
+      headers: createAuthHeader(),
+    },
+  );
 }
 
 /**
--- apps/web-antd/src/preferences.ts
+++ apps/web-antd/src/preferences.ts
@@ -11,3 +11,12 @@ export const overridesPreferences = defineOverridesPreferences({
     name: import.meta.env.VITE_APP_TITLE,
   },
 });
+
+export const APP = {
+  OAUTH: {
+    // oauth中请求头内需加密的client_id
+    client_id: 'c85b4d4e6d894c2a9fb6719c8875e114',
+    // oauth中请求头内需加密的client_secret
+    client_secret: 'af6814bed3fd435a8cd46cea00be2e80',
+  },
+};
